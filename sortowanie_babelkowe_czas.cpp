a4199853a0741cf92cb45e4a029072a051723a08
MrKamirus <61318695+MrKamirus@users.noreply.github.com>
2023-01-28 17:36:18 +0100

    Update sortowanie_babelkowe_czas.cpp

--- sortowanie_babelkowe_czas.cpp
+++ sortowanie_babelkowe_czas.cpp
@@ -28,26 +28,26 @@ int main()
         //cout << endl;
         int a = 0;
         auto start = chrono::steady_clock::now();
-		for (i = 0; i < N; i++) d[i] = rand() % 100;
-		for (i = 0; i < N; i++) cout << setw(4) << d[i];
-		cout << endl;
+	for (i = 0; i < N; i++) d[i] = rand() % 100;
+	for (i = 0; i < N; i++) cout << setw(4) << d[i];
+	cout << endl;
 		// Sortujemy
-		smin = 0; smax = N - 1;
-		do
-		{
-			s = -1;
-			for (i = smin; i < smax; i++) {
-				if (d[i] > d[i + 1])
-				{
-					swap(d[i], d[i + 1]); // Zamieniamy elementy ze sobą
-					if (s < 0) smin = i; // Ustalamy dolną granicę
-					sortowanych elementów
-						s = i;
-				}
+	smin = 0; smax = N - 1;
+	do
+	{
+		s = -1;
+		for (i = smin; i < smax; i++) {
+			if (d[i] > d[i + 1])
+			{
+				swap(d[i], d[i + 1]); // Zamieniamy elementy ze sobą
+				if (s < 0) smin = i; // Ustalamy dolną granicę
+				sortowanych elementów
+					s = i;
 			}
-			smin--;
-			smax = s;
-		} while (s >= 0);
+		}
+		smin--;
+		smax = s;
+	} while (s >= 0);
         auto end = chrono::steady_clock::now();
         //auto czas = chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
         //double czas = czas.count() * 1e-9;
